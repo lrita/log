559d3c8ca761a11b2c8d3b3a3967dc9328111827
lrita <lrita@163.com>
2023-03-23 19:05:30 +0800

    support fadvise for all linux

--- fadvise_linux.go
+++ fadvise_linux.go
@@ -1,14 +1,14 @@
+//go:build linux
 // +build linux
-// +build 386 amd64
 
 package log
 
 import (
 	"os"
-	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 func fadvise(f *os.File) error {
-	_, _, err := syscall.Syscall6(syscall.SYS_FADVISE64, f.Fd(), 0, 0, 4, 0, 0)
-	return err
+	return unix.Fadvise(int(f.Fd()), 0, 0, unix.FADV_DONTNEED)
 }
--- go.mod
+++ go.mod
@@ -13,5 +13,6 @@ require (
 	github.com/intel-go/cpuid v0.0.0-20220614022739-219e067757cb // indirect
 	github.com/lrita/numa v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
